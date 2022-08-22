f01f4576fe7a88627bf69eecaf026a892f21d873
seungheonkim <seungheon818@naver.com>
2022-08-22 10:51:42 +0900

    typeOf

---
+++ javascript/type_Of.js
@@ -0,0 +1,9 @@
+console.log(typeof 1)//number
+console.log(typeof '1')//string
+console.log(typeof (1 < 2))//boolean -> true
+
+let num = 12;
+console.log(typeof num)
+
+let str = 'hello';
+console.log(typeof str)
