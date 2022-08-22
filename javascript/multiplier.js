886223ae2518b1e962c08562bcca0769a024c086
seungheonkim <seungheon818@naver.com>
2022-08-22 10:19:13 +0900

    multiplier

---
+++ javascript/multiplier.js
@@ -0,0 +1,25 @@
+//구구단 2단 출력
+let num_2 = 2;
+
+console.log(num_2 * 1)
+console.log(num_2 * 2)
+console.log(num_2 * 3)
+console.log(num_2 * 4)
+console.log(num_2 * 5)
+console.log(num_2 * 6)
+console.log(num_2 * 7)
+console.log(num_2 * 8)
+console.log(num_2 * 9)
+
+//구구단 3단 출력
+let num_3 = 3;
+
+console.log(num_3 * 1)
+console.log(num_3 * 2)
+console.log(num_3 * 3)
+console.log(num_3 * 4)
+console.log(num_3 * 5)
+console.log(num_3 * 6)
+console.log(num_3 * 7)
+console.log(num_3 * 8)
+console.log(num_3 * 9)
