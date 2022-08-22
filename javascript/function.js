b40cbe5e48c8471b80683034601a926b05aba9b6
seungheonkim <seungheon818@naver.com>
2022-08-22 11:19:44 +0900

    fun_multiple

---
+++ javascript/function.js
@@ -0,0 +1,16 @@
+//함수를 이용하여 구구단 출력하기
+function multiplier(num) {
+    console.log(num * 1);
+    console.log(num * 2);
+    console.log(num * 3);
+    console.log(num * 4);
+    console.log(num * 5);
+    console.log(num * 6);
+    console.log(num * 7);
+    console.log(num * 8);
+    console.log(num * 9);
+}
+
+multiplier(2);
+multiplier(3);
+multiplier(4);
