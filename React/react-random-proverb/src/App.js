fd0b50809e73efe8332cfed4a35acb798c45196e
seungheonkim <seungheon818@naver.com>
2022-09-28 11:30:56 +0900

    proverbList_show

--- React/react-random-proverb/src/App.js
+++ React/react-random-proverb/src/App.js
@@ -6,11 +6,19 @@ function App() {
         '누워서 떡먹기',
         '낫 놓고 기억자 모른다',
         '친구따라 강남간다',
+        '소 잃고 외양간 고친다'
     ]
 
     const getRandomIndex = (length) => {
         return parseInt(Math.random() * length);
     }
+
+    const showAllProverbs = (proverb) => {
+        return (
+            <p>{proverb}</p>
+        )
+    }
+
     return (
         <div className="App">
             <header className="App-header">
@@ -18,14 +26,8 @@ function App() {
                 <p>
                     Edit <code>src/App.js</code> 파일고치고 저장하면 새로고침됩니다.
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    {proverbs[getRandomIndex(proverbs.length)]}
-                </a>
+                {/*{proverbs[getRandomIndex(proverbs.length)]}*/}
+                {proverbs.map(showAllProverbs)}
             </header>
         </div>
     );
