60058a657f7b40308e7d5b42baeb7f55a834dc5a
seungheonkim <seungheon818@naver.com>
2022-09-28 11:19:43 +0900

    react_twitt_intro

--- React/react-random-proverb/src/App.js
+++ React/react-random-proverb/src/App.js
@@ -2,24 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const proverbs = [
+        '누워서 떡먹기',
+        '낫 놓고 기억자 모른다',
+        '친구따라 강남간다',
+    ]
+
+    const getRandomIndex = (length) => {
+        return parseInt(Math.random() * length);
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> 파일고치고 저장하면 새로고침됩니다.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {proverbs[getRandomIndex(proverbs.length)]}
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
