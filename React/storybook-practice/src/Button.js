580c3cee5286a29e6df133b1772905bc35cbcfd4
seungheonkim <seungheon818@naver.com>
2022-10-27 18:28:28 +0900

    react_cdd_added

--- React/storybook-practice/src/Button.js
+++ React/storybook-practice/src/Button.js
@@ -6,8 +6,8 @@ const StyledButton = styled.button`
   background: ${(props) => props.color || 'white'};
   //props.size 가 big 이면 200px, 그렇지 않으면 100px 를 너비로 사용
   width: ${(props) => (props.size === 'big' ? '200px' : '100px')};
-  //props.size 가 big 이면 80px, 아니면 4px를 높이로 사용
-  height: ${(props) => (props.size === 'big' ? '80px' : '4px')};
+  //props.size 가 big 이면 80px, 아니면 40px를 높이로 사용
+  height: ${(props) => (props.size === 'big' ? '80px' : '40px')};
 `;
 
 export const Button = ({color, size, text}) => (
