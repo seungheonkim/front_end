16f74d17eacb1c2a8d240240e2d5c54eb5cb5766
seungheonkim <seungheon818@naver.com>
2022-11-14 00:34:21 +0900

    section5_done

--- React/Lecture/01-starting-setup/src/components/Expenses/Expenses.js
+++ React/Lecture/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,27 +1,27 @@
 import './Expenses.css';
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import {useState} from "react";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
-const Expenses = ({items}) => {
-    const [filteredYear, setFilteredYear] = useState('2020');
+const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2022');
 
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
-    }
+    };
+
+    const filteredExpenses = props.items.filter((item) => {
+        return item.date.getFullYear().toString() === filteredYear;
+    });
 
     return (
         <div>
             <Card className={'expenses'}>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {items.map((item) => (
-                    <ExpenseItem
-                        key={item.id}
-                        date={item.date}
-                        title={item.title}
-                        amount={item.amount}/>
-                ))}
+                <ExpensesChart expenses={filteredExpenses}/>
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
     )
--- React/Lecture/01-starting-setup/src/components/NewExpense/NewExpense.js
+++ React/Lecture/01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,18 +1,34 @@
 import './NexExpense.css'
 import ExpenseForm from "./ExpenseForm";
+import {useState} from "react";
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsEditing(false);
+    }
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    const stopEditing = () => {
+        setIsEditing(false);
     }
 
     return (
         <div className={'new-expense'}>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && <ExpenseForm
+                onSaveExpenseData={saveExpenseDataHandler}
+                onCancel={stopEditing}
+            />}
         </div>
     )
 };
