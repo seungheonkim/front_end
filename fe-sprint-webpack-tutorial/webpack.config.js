542782fbf10b1d0151909410f6459301d67e43f5
seungheonkim <seungheon818@naver.com>
2022-11-23 14:00:25 +0900

    tutorial2_done

--- fe-sprint-webpack-tutorial/webpack.config.js
+++ fe-sprint-webpack-tutorial/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "app.bundle.js",
+        filename: "main.js",
     },
     module: {
         rules: [
