d8024e7711d48b162b6cb8bcd9139646b52f0781
seungheonkim <seungheon818@naver.com>
2022-11-30 23:49:29 +0900

    useCallback

--- React/Lecture/section12/src/App.js
+++ React/Lecture/section12/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 
 import './App.css';
 import Button from "./components/UI/Button/Button";
@@ -6,19 +6,26 @@ import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
     const [showPara, setShowPara] = useState(false);
+    const [allowToggle, setAllowToggle] = useState(false);
 
     console.log('App Running!!');
 
-    const toggleParagraphHandler = () => {
-        setShowPara(prevShowPara => !prevShowPara);
-    };
+    //save a function to reuse it without re-rendering
+    const toggleParagraphHandler = useCallback(() => {
+        if (allowToggle) {
+            setShowPara(prevShowPara => !prevShowPara);
+        }
+    }, [allowToggle]);
 
-
+    const allowToggleHandler = () => {
+        setAllowToggle(true);
+    }
 
     return (
         <div className="app">
             <h1>Hi there!</h1>
-            <DemoOutput show={false}/>
+            <DemoOutput show={showPara}/>
+            <Button onClick={allowToggleHandler}>Allow Toggling</Button>
             <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
         </div>
     );
