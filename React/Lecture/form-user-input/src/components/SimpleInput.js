964a531a44feb615f60609cf440d708ce8b0e233
seungheonkim <seungheon818@naver.com>
2022-12-05 23:00:19 +0900

    done_Basic_Form

---
+++ React/Lecture/form-user-input/src/components/SimpleInput.js
@@ -0,0 +1,77 @@
+import useInput from "../hooks/use-input";
+
+const SimpleInput = (props) => {
+    const {
+        value: enteredName,
+        isValid: enteredNameIsValid,
+        hasError: nameInputHasError,
+        valueChangeHandler: nameChangedHandler,
+        inputBlurHandler: nameBlurHandler,
+        reset: resetNameInput
+    } = useInput((value) => value.trim().length !== 0);
+
+    const {
+        value: enteredEmail,
+        isValid: enteredEmailIsValid,
+        hasError: emailInputHasError,
+        valueChangeHandler: emailChangedHandler,
+        inputBlurHandler: emailBlurHandler,
+        reset: resetEmailInput
+    } = useInput((value) => value.trim().includes('@'));
+
+    let formIsValid = false;
+
+    if (enteredNameIsValid && enteredEmailIsValid) {
+        formIsValid = true;
+    }
+
+    const formSubmissionHandler = (event) => {
+        //상태를 유지하기 위해 새로고침 방지
+        event.preventDefault();
+
+        if (!formIsValid) {
+            return;
+        }
+
+        console.log(`from state: ${enteredName}`);
+        console.log(`from state: ${enteredEmail}`);
+
+        resetNameInput();
+        resetEmailInput();
+    };
+
+    const nameInputClasses = nameInputHasError ? 'form-control invalid' : 'form-control';
+    const emailInputClasses = emailInputHasError ? 'form-control invalid' : 'form-control';
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className={nameInputClasses}>
+                <label htmlFor='name'>Your Name</label>
+                <input
+                    type='text'
+                    id='name'
+                    onChange={nameChangedHandler}
+                    onBlur={nameBlurHandler}
+                    value={enteredName}
+                />
+                {nameInputHasError && <p className={'error-text'}>Name must not be empty!!</p>}
+            </div>
+            <div className={emailInputClasses}>
+                <label htmlFor='name'>Your Email</label>
+                <input
+                    type='email'
+                    id='email'
+                    onChange={emailChangedHandler}
+                    onBlur={emailBlurHandler}
+                    value={enteredEmail}
+                />
+                {emailInputHasError && <p className={'error-text'}>Email must includes @!!</p>}
+            </div>
+            <div className="form-actions">
+                <button disabled={!formIsValid}>Submit</button>
+            </div>
+        </form>
+    );
+};
+
+export default SimpleInput;
